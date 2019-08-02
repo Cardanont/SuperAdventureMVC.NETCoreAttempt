ab6c9553bd864caf8f1d0f636890e5e2b06885d7
Daniel Ontiveros <cardanont@gmail.com>
2019-08-01 23:47:52 -0500

    Added Monster and Weapon Models

---
+++ Models/Weapon.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperAdventure.Models
+{
+    public class Weapon
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public string NamePlural { get; set; }
+        public int MinimumDamage { get; set; }
+        public int MaximumDamage { get; set; }
+    }
+}

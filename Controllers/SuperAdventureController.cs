fbd4f3a0e565ac2e7e75dc56bc9e9bce17e8fd52
Daniel Ontiveros <cardanont@gmail.com>
2019-08-01 23:38:24 -0500

    Refactoring all to MVC type Adding player on Model

--- Controllers/SuperAdventureController.cs
+++ Controllers/SuperAdventureController.cs
@@ -2,8 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
-using Engine;
 using Microsoft.AspNetCore.Mvc;
+using SuperAdventure.Models;
 
 namespace SuperAdventure.Controllers
 {
---
+++ Models/Player.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SuperAdventure.Models
+{
+    public class Player
+    {
+        [Display(Name = "Current Hit Points")]
+        public int CurrentHitPoints { get; set; }
+        [Display(Name = "Maximum Hit points")]
+        public int MaximumHitPoints { get; set; }
+        [Display(Name = "Gold")]
+        public int Gold { get; set; }
+        [Display(Name = "Experience Points")]
+        public int ExperiencePoints { get; set; }
+        [Display(Name = "Level")]
+        public int Level { get; set; }
+    }
+}

2c06b1ad9422fee3d0b826017f4af0f63bc26405
Daniel Ontiveros <cardanont@gmail.com>
2019-08-01 23:27:03 -0500

    Add project files.

---
+++ Controllers/SuperAdventureController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Engine;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SuperAdventure.Controllers
+{
+    public class SuperAdventureController : Controller
+    {
+        public SuperAdventureController()
+        {
+            
+        }
+
+        public IActionResult Index()
+        {
+            Player player = new Player();
+
+            player.CurrentHitPoints = 10;
+            player.MaximumHitPoints = 10;
+            player.Gold = 20;
+            player.ExperiencePoints = 0;
+            player.Level = 1;
+
+            return View(player);
+        }
+    }
+}
